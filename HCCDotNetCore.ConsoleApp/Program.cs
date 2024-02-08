4efbd908289c2aa077e50f7cb53c8920e53f68a8
hmwaychocho <hmwaychocho.tze@gmail.com>
2024-02-08 16:09:41 +0630

    Add project files.

---
+++ HCCDotNetCore.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = ".";
+sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "sa@123";
+
+SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+connection.Open();
+
+string query=@"SELECT [BlogId],[BlogTitle],[BlogAuthor] ,[BlogContent]
+     FROM [dbo].[Tbl_Blog]";
+DataTable dt = new DataTable();
+
+SqlCommand command=new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(command);
+adapter.Fill(dt);
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["BlogId"]);
+    Console.WriteLine(dr["BlogTitle"]);
+    Console.WriteLine(dr["BlogAuthor"]);
+    Console.WriteLine(dr["BlogContent"]);
+}
+Console.ReadLine();
